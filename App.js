973ca27b8d33d1f7415e60cc510f4744e8138034
Volodymyr Smahliuk <smaglukvolodia54@gmail.com>
2024-08-03 18:35:45 +0300

    final

--- App.js
+++ App.js
@@ -5,53 +5,9 @@ import GoalInput from "./components/GoalInput";
 import CustomButton from "./components/CustomButton";
 import GoalsList from "./components/GoalsList";
 
-const arrayOfObjects = [
-  {
-    text: "Покращити навички програмування",
-    id: Math.random().toString(),
-  },
-  {
-    text: "Вивчити нову мову програмування",
-    id: Math.random().toString(),
-  },
-  {
-    text: "Завершити проект у строк",
-    id: Math.random().toString(),
-  },
-  {
-    text: "Оптимізувати код існуючого проекту",
-    id: Math.random().toString(),
-  },
-  {
-    text: "Підвищити рівень знань у веб-розробці",
-    id: Math.random().toString(),
-  },
-  {
-    text: "Вивчити нові технології фронтенду",
-    id: Math.random().toString(),
-  },
-  {
-    text: "Поліпшити навички роботи з базами даних",
-    id: Math.random().toString(),
-  },
-  {
-    text: "Створити власний блог",
-    id: Math.random().toString(),
-  },
-  {
-    text: "Покращити продуктивність коду",
-    id: Math.random().toString(),
-  },
-  {
-    text: "Знайти нові цікаві проекти для співпраці",
-    id: Math.random().toString(),
-  },
-];
-
 export default function App() {
   const [modalIsVisible, setModalIsVisible] = useState(false);
-  const [courseGoals, setCourseGoals] = useState(arrayOfObjects);
-  // const [courseGoals, setCourseGoals] = useState([]);
+  const [courseGoals, setCourseGoals] = useState([]);
 
   const startAddGoalHandler = () => {
     setModalIsVisible(true);
